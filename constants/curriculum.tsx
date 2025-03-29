8f1aea7818e7140fa9efefdc5a11df643c9a4cdf
Jeff Mofor <allojeff444@gmail.com>
2025-03-29 11:10:02 -0400

    Add curriculum

---
+++ constants/curriculum.tsx
@@ -0,0 +1,142 @@
+const curriculum = [
+  {
+    week: "Week 1: Introduction to PCB Design",
+    content: ["How to solder", "What is HOPE?", "HOPE curriculum overview"],
+    labs: ["Sodering lab"],
+    resourses: [
+      {
+        name: "slides",
+        url: "https://docs.google.com/presentation/d/1rqhQ1w4423QwQMLXVl85t_rP3os7dm_b/edit?usp=sharing&ouid=100652969463088965516&rtpof=true&sd=true",
+      },
+    ],
+    deliverables: [],
+  },
+  {
+    week: "Week 2: KiCAD",
+    content: [
+      "What is a PCB?",
+      "Why learn PCB design?",
+      "PCB design tools",
+      "PCB design flow",
+      "PCB schematic capture and layout flow",
+      "Intro to KiCAD",
+      "KiCAD design flow",
+      "KiCAD schematic capture editor",
+    ],
+    labs: ["Install KiCAD", "Create a simple schematic"],
+    resourses: [
+      {
+        name: "slides",
+        url: "https://docs.google.com/presentation/d/1W1NBigy0Wf6DIvTPeIjClLoXQP8KvupU/edit?usp=sharing&ouid=100652969463088965516&rtpof=true&sd=true",
+      },
+    ],
+    deliverables: [
+      "Complete proximity sensor schematic",
+      "Final project team formation",
+    ],
+  },
+  {
+    week: "Week 3: Schematic Capture and PCB Layout",
+    content: [
+      "Schematic conventions",
+      "Nets",
+      "KiCAD layout editor",
+      "Footprints",
+      "Layout features",
+    ],
+    labs: ["Create a simple PCB layout"],
+    resourses: [
+      {
+        name: "slides",
+        url: "https://docs.google.com/presentation/d/1M4wTYanop1ngZkNOIlWPy3ttMGkNqe7b/edit?usp=sharing&ouid=100652969463088965516&rtpof=true&sd=true",
+      },
+      {
+        name: "KiCAD layout tutorial",
+        url: "https://youtu.be/7FXaS7H3_CY"
+      }
+    ],
+    deliverables: [
+      "Complete proximity sensor layout",
+      "Possible final project ideas and feedback",
+    ],
+  },
+  {
+    week: "Week 4: Components",
+    content: ["Components and component selection", "What is a BOM?"],
+    labs: ["Solder proximity sensor", "Work on final project"],
+    resourses: [
+      {
+        name: "slides",
+        url: "https://docs.google.com/presentation/d/154-Ct5p871eiHZ0E_u5A5MSuk8VvvBZQ/edit?usp=sharing&ouid=100652969463088965516&rtpof=true&sd=true",
+      },
+    ],
+    deliverables: [
+      "Solder proximity sensor",
+      "Final project proposal submission",
+    ],
+  },
+  {
+    week: "Week 5: Design Abstraction",
+    content: ["What is design abstraction?", "PCB design process"],
+    labs: ["Soldering proximity sensor/audio amplifier"],
+    resourses: [
+      {
+        name: "slides",
+        url: "https://docs.google.com/presentation/d/1hVW8C3VJ1OcBMdmPJ3OfMr9jQFHAFo_A/edit?usp=sharing&ouid=100652969463088965516&rtpof=true&sd=true",
+      },
+    ],
+    deliverables: [
+      "Complete proximity sensor layout",
+      "Final project block diagram submission",
+    ],
+  },
+  {
+    week: "Week 6: Audio amplifiers",
+    content: [
+      "Functional abstraction in schematics",
+      "Audio amplifiers",
+      "Audio amplifier circuit/schematic",
+    ],
+    labs: ["Create audio amplifier schematic"],
+    resourses: [
+      {
+        name: "slides",
+        url: "https://docs.google.com/presentation/d/1OHWP8kvkGCXqycU5Um_4OxrhOGTki3j5/edit?usp=sharing&ouid=100652969463088965516&rtpof=true&sd=true",
+      },
+    ],
+    deliverables: [
+      "Complete audio amplifier schematic",
+      "Final project schematic submission",
+    ],
+  },
+  {
+    week: "Week 6: Microcontrollers",
+    content: ["Microcontrollers"],
+    labs: ["Create audio amplifier layout", "Solder audio amplifier PCB"],
+    resourses: [
+      {
+        name: "slides",
+        url: "https://drive.google.com/drive/folders/1aL-iP8HF3XhMsMMUBLJDS7dZ-f5CKg_N?usp=drive_link",
+      },
+    ],
+    deliverables: [
+      "Complete audio amplifier layout",
+      "Soldered Audio amp",
+      "Final project layout submission",
+    ],
+  },
+  {
+    week: "Week 8: Project showcase",
+    content: ["Showcase final projects"],
+    labs: [],
+    resourses: [
+      {
+        name: "slides",
+        url: "https://drive.google.com/drive/folders/1aL-iP8HF3XhMsMMUBLJDS7dZ-f5CKg_N?usp=drive_link",
+      },
+    ],
+    deliverables: [],
+  },
+];
+
+export default curriculum;
