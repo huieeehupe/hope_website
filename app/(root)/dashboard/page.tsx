80dc420fb4a45d8aabc2a0fdde02933a9fd887ed
Jeff Mofor <allojeff444@gmail.com>
2025-03-30 11:29:42 -0400

    Removed unused imports

--- app/(root)/dashboard/page.tsx
+++ app/(root)/dashboard/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { redirect } from "next/navigation";
 import { auth } from "@/auth";
-import SignOutButton from "@/components/signOutButton";
 
 const DashboardPage = async () => {
   // Check if the user is authenticated
