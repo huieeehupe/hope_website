59674df3bea0fc321ab397df596695e86d66c34c
Jeff Mofor <allojeff444@gmail.com>
2025-05-07 21:22:45 -0400

    Fix auth.ts

--- auth.ts
+++ auth.ts
@@ -3,7 +3,7 @@ import Google from "next-auth/providers/google";
 import GitHub from "next-auth/providers/github";
 import EmailProvider from "next-auth/providers/nodemailer";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "@/lib/prisma";
+import prisma from "@/lib/prisma";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
